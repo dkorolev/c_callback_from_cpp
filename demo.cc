cb437b372b531db274c2ef19d6658c5ff8873984
Dima Korolev <dmitry.korolev+github-2022@gmail.com>
2024-08-03 15:46:46 +0200

    The `using` directive for the type.

--- demo.cc
+++ demo.cc
@@ -5,9 +5,15 @@ void call_c_callback(void (*p)()) {
 }
 
 void c_print_foo() {
-  std::cout << "foo" << std::endl;
+  static int c = 0;
+  std::cout << "foo, call counter: " << ++c << std::endl;
 }
+
+using c_callback_t = void (*)();
 
 int main() {
   call_c_callback(c_print_foo);
+
+  c_callback_t const p_c_print_foo = c_print_foo;
+  call_c_callback(p_c_print_foo);
 }
