6941bec77c2bdda6d101e7706d42a91e29b52ebb
Dima Korolev <dmitry.korolev+github-2022@gmail.com>
2024-08-03 15:48:15 +0200

    Here is how to do it with static data, w/o `this`.

--- demo.cc
+++ demo.cc
@@ -11,9 +11,25 @@ void c_print_foo() {
 
 using c_callback_t = void (*)();
 
+struct CppClassWithData final {
+  std::string const s_;
+  c_callback_t const c_callback;
+
+  explicit CppClassWithData(std::string s) : s_(std::move(s)), c_callback(StaticF) {}
+
+  static void StaticF() {
+    std::cout << "CppClassWithData::StaticF() called, stateless so far." << std::endl;
+  }
+};
+
 int main() {
   call_c_callback(c_print_foo);
 
   c_callback_t const p_c_print_foo = c_print_foo;
   call_c_callback(p_c_print_foo);
+
+  CppClassWithData bar("bar");
+  CppClassWithData baz("bar");
+  call_c_callback(bar.c_callback);
+  call_c_callback(baz.c_callback);
 }
